3d07ff61ea4f52382c5d93ea4a3d4e370dff22b0
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-09-22 22:13:24 +0200

    fix: progress bar om skill

--- components/SkillItem.jsx
+++ components/SkillItem.jsx
@@ -1,49 +1,29 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import styles from './SkillItem.module.scss';
 
 export default class SkillItem extends React.Component {
     render() {
-        let { img, color, value } = this.props
+        let { img, color, value } = this.props;
         return (
-            <div className="column is-2-desktop has-text-center" style={{'display': 'flex'}}>
-                <div className={'circle c100 p' + value + ' dark big orange'}>
-                    <span>
-                        <Image src={img} width="60" height="60" alt=""/>
-                    </span>
-                    <div className="slice">
-                        <div className="bar"></div>
-                        <div className="fill"></div>
+            <div className="column is-2-desktop has-text-centered">
+                <div className={styles.container}>
+                    <div className={styles.progressBarContainer}>
+                        <CircularProgressbar
+                            value={value}
+                            styles={buildStyles({
+                                pathColor: color,
+                                trailColor: '#d6d6d6',
+                            })}
+                        />
+                        <div className={styles.imageContainer}>
+                            <Image src={img} width="60" height="60" alt="" />
+                        </div>
                     </div>
-                    <style jsx>{`
-                        img {
-                            margin-top: 30px;
-                            max-width: 60px;
-                            -webkit-transition: -webkit-transform 0.5s ease-in-out;
-                            transition: transform 0.5s ease-in-out;
-                        }
-                        .circle:hover img {
-                            -webkit-transform: rotate(-720deg);
-                            transform: rotate(-720deg);
-                        }
-                        .bar {
-                            border-color: ${color} !important;
-                        }
-                        .fill {
-                            border-color: ${color} !important;
-                        }
-                        .circle {
-                            margin: 10px auto;
-                            -webkit-transition: -webkit-transform 0.5s ease-in-out;
-                            transition: transform 0.5s ease-in-out;
-                        }
-                        .circle:hover {
-                            -webkit-transform: rotate(360deg) scale(1.4);
-                            transform: rotate(360deg) scale(1.4);
-                            z-index: 999;
-                        }
-                    `}</style>
                 </div>
             </div>
-        )
+        );
     }
 }
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import circle from '../styles/circle.module.css';
 import Group from './SkillItemGroup';
+import styles from './Skills.module.scss';
 import Title from './Title';
 
 export default class Skills extends React.Component {
@@ -12,65 +12,43 @@ export default class Skills extends React.Component {
                     { name: 'html5', color: '#f16529', value: '75' },
                     { name: 'css3', color: '#29a9df', value: '70' },
                     { name: 'sass', color: '#cb6699', value: '50' },
-                    { name: 'bootstrap', color: '#8b57d9', value: '80' },                
-                    { name: 'javascript', color: '#f7df1e', value: '70' },
-                    { name: 'jquery', color: '#288cc4', value: '60' },
-                    { name: 'angularjs', color: '#dd0330', value: '60' },
-                    { name: 'reactjs', color: '#61d9fa', value: '45' },
-                    { name: 'vuejs', color: '#41b883', value: '25' },
+                    { name: 'bootstrap', color: '#8b57d9', value: '30' },
+                    { name: 'javascript', color: '#f7df1e', value: '30' },
+                    { name: 'reactjs', color: '#61d9fa', value: '50' },
                 ]
             },
             {
                 name: 'Back-ends',
                 list: [
-                    { name: 'php', color: '#8993be', value: '70' },           
-                    { name: 'nodejs', color: '#81ca2a', value: '70' },
-                    { name: 'java', color: '#1e77b7', value: '70' },
-                    { name: 'rails', color: '#e74c3c', value: '60' }
+                    { name: 'php', color: '#8993be', value: '80' },
+                    { name: 'nodejs', color: '#81ca2a', value: '75' },
                 ]
             },
             {
                 name: 'Databases',
                 list: [
                     { name: 'mysql', color: '#0784af', value: '65' },
-                    { name: 'firebase', color: '#ffcb2c', value: '55' }
                 ]
             },
             {
                 name: 'Tools',
                 list: [
                     { name: 'git', color: '#F05032', value: '60' },
-                    { name: 'yarn', color: '#2c8ebb', value: '40' },
                     { name: 'npm', color: '#cb3736', value: '55' },
-                    { name: 'webpack', color: '#6dcdff', value: '25' },
-                    { name: 'rundeck', color: '#ff0000', value: '45' },
-                    { name: 'elasticsearch', color: '#18bbb0', value: '40' },
                 ]
-            } 
+            }
         ]
 
         return (
-            <section className="hero wrapper has-text-centered">
-                <style dangerouslySetInnerHTML={{ __html: circle }} />
+            <section className={`hero ${styles.wrapper} has-text-centered`}>
                 <Title title="Skills" color='#ffffff'/>
                 <div className="container">
                     {
                         skillImgs.map((item, key) => (
-                            <Group key={key} name={item.name} items={item.list} />    
+                            <Group key={key} name={item.name} items={item.list} />
                         ))
                     }
                 </div>
-                
-                <style jsx>{`
-                    .wrapper {
-                        padding-top: 50px;
-                        padding-bottom: 50px;
-                        background: #2f353f;
-                        background-size: cover;
-                        color: #333333;
-                        font-size: 1.3em;
-                    }
-                `}</style>
             </section>
         )
     }
