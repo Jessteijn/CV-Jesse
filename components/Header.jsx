ab26c0fe6fe6959f968687de3ac5842a008b3d64
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-09-22 20:46:08 +0200

    add: particle background

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-// import Background from './HeaderBackground';
 import Brace from './HeaderBrace';
 import Button from './HeaderButton';
 import ProfileImage from './HeaderProfile';
 import Title from './HeaderTitle';
+import ParticlesBackground from './ParticlesBackground';
+
 const themes = {
     light: {
         background: '#f5f5f5',
@@ -13,48 +14,50 @@ const themes = {
         background: '#2f353f',
         font: '#e4e4e4'
     }
-}
+};
 
 export default class Header extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
-            theme: 'light'
-        }
+            theme: 'dark'
+        };
     }
 
     toggleTheme() {
-        let newtheme = this.state.theme === 'light' ? 'dark' : 'light'
+        let newtheme = this.state.theme === 'light' ? 'dark' : 'light';
         this.setState({
             theme: newtheme
-        })
+        });
     }
 
     render() {
         let theme = themes[this.state.theme];
         return (
-            <section className="hero is-fullheight has-text-centered">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-mobile">
-                            <Brace type="left" color={theme.font}/>
-                            <ProfileImage toggle={this.toggleTheme.bind(this)} />
-                            <Brace type="right" color={theme.font}/>
-                        </div>
-                        <div className="columns">
-                            <div className="column">
-                                <Title color={theme.font}/>
+            <>
+                <ParticlesBackground />
+                <section className="hero is-fullheight has-text-centered">
+                    <div className="hero-body">
+                        <div className="container">
+                            <div className="columns is-mobile">
+                                <Brace type="left" color={theme.font} />
+                                <ProfileImage toggle={this.toggleTheme.bind(this)} />
+                                <Brace type="right" color={theme.font} />
                             </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column">
-                                <Button title="My GitHub" url="https://github.com/Jessteijn" />                             
+                            <div className="columns">
+                                <div className="column">
+                                    <Title color={theme.font} />
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <Button title="My GitHub" url="https://github.com/Jessteijn" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    {/* <Background color={theme.background}/> */}
-                </div>
-            </section>
-        )
+                </section>
+            </>
+        );
     }
 }
---
+++ components/ParticlesBackground.jsx
@@ -0,0 +1,42 @@
+import { loadFirePreset } from "@tsparticles/preset-fire";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+import styles from "./ParticlesBackground.module.scss";
+
+const ParticlesBackground = () => {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+      await loadFirePreset(engine); // Load the fire preset
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(() => ({
+    preset: "fire",
+  }), []);
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        className={styles.particles}
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticlesBackground;
