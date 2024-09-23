516093099294a4c837ded066068a5eded58b97bc
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-09-23 19:42:48 +0200

    upd: use bulma link, not file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import '../styles/bulma.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css?family=Questrial&display=optional" rel="stylesheet" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css" />
         </Head>
         <body>
             {this.props.customValue}
