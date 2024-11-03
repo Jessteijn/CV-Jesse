6385fe4adc483a9f3d06b7a305d6aa2039628f97
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-11-03 22:35:43 +0100

    add: loadscreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
+"use client";
 import About from "@/Components/About";
 import Education from "@/Components/Education";
 import Experiences from "@/Components/Experiences";
 import Header from "@/Components/Header";
 import Skills from "@/Components/Skills";
 
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Resume | Jesse Wessteijn",
-};
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <About />
-      <Experiences />
-      <Education />
-      <Skills />
-    </div>
+    <>
+      {mounted ? (
+        <>
+          <Header />
+          <About />
+          <Experiences />
+          <Education />
+          <Skills />
+        </>
+      ) : (
+        <section className="flex h-screen items-center justify-center">
+          <div className="text-center text-2xl">Loading...</div>
+        </section>
+      )}
+    </>
   );
 }
