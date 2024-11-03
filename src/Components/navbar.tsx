d20ff452f0688ad07b7d9fad6927840e1f62c65d
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-11-03 22:39:38 +0100

    fix: version and eslint

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 bg-background">
+    <header className="bg-background sticky top-0">
       <div className="absolute right-0 gap-4 p-10">
         <ThemeToggle />
       </div>
