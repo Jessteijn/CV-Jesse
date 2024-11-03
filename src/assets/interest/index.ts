b11183734d7dc3643f5ea0a227fca159a352665d
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-11-03 20:42:06 +0100

    add: cleanup and add relevent interest

--- src/assets/interest/index.ts
+++ src/assets/interest/index.ts
@@ -1,8 +1,12 @@
 import { interestProps } from "@/types/interest-types";
-import programming from "./interest-0.svg";
-import reading from "./interest-1.svg";
+import boarding from "./boarding.svg";
+import programming from "./programming.svg";
+import reading from "./reading.svg";
+import scouting from "./scouting-logo.svg";
 
 export const interest: interestProps[] = [
   { name: "Programming", img: programming },
   { name: "Reading", img: reading },
+  { name: "Scouting", img: scouting },
+  { name: "Board Sports", img: boarding },
 ];
