5d6d4c24bbc6d1397152083ef18c2bb444d86b26
Jesse Wessteijn <jessteijn@hotmail.nl>
2024-11-05 21:27:23 +0100

    fix: title render on phone

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -6,7 +6,7 @@ import { pageProps } from "@/types/page-type";
 export default function Skills({ className }: pageProps) {
   return (
     <section className={`${className} py-12 text-center text-xl`}>
-      <Title>Vaardigheden</Title>
+      <Title className="text-4xl sm:text-5xl">Vaardigheden</Title>
       {skillImgs.map((item, key) => (
         <Group key={key} name={item.name} items={item.items} />
       ))}
--- src/Components/Title.tsx
+++ src/Components/Title.tsx
@@ -1,7 +1,12 @@
 interface TitleProps {
+  className?: string;
   children: React.ReactNode;
 }
 
-export default function Title({ children }: TitleProps) {
-  return <h1 className="mb-8 text-5xl font-bold uppercase">{children}</h1>;
+export default function Title({ className, children }: TitleProps) {
+  return (
+    <div className="text-5xl">
+      <h1 className={`${className} mb-8 font-bold uppercase`}>{children}</h1>
+    </div>
+  );
 }
